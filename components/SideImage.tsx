2d27af280a28aefb60e8aeb0840eb865319e1814
Vbeni <benivillafana@gmail.com>
2023-08-17 17:20:17 -0700

    mostly done

---
+++ components/SideImage.tsx
@@ -0,0 +1,9 @@
+// components/SideImage.tsx
+export default function SideImage() {
+    return (
+        <div className='sidebar-image'>
+      <img src="/images/black-n-white.png" alt="Description of Image" className="sidebar-image" />
+      </div>
+    );
+  }
+  
