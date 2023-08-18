7ae6c92c6920680288bce4aa95ccf047320eacc6
Vbeni <benivillafana@gmail.com>
2023-08-17 18:49:04 -0700

    api issues

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    env: {
+      OPENAI_API_KEY: process.env.API_KEY,
+    },
+  };
+  
