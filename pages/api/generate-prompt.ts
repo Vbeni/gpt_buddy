1f6f2e2e83edd5deed13661fae51cb325005fa84
Vbeni <benivillafana@gmail.com>
2023-08-17 17:26:32 -0700

    deployment fixes

--- pages/api/generate-prompt.ts
+++ pages/api/generate-prompt.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+const generatePrompt = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method !== 'POST') {
       return res.status(405).end();
     }
@@ -37,3 +37,5 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     }
     
 };
+
+export default generatePrompt;
